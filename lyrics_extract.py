f0ecd06b1cc9ae759f9a144ead0b70d60ff34e67
Vedant <96318619+Vedant-rai@users.noreply.github.com>
2022-07-31 01:53:56 +0530

    Update lyrics_extract.py

--- lyrics_extract.py
+++ lyrics_extract.py
@@ -6,11 +6,11 @@ artist = input("Enter artist name: ")
 #construct url
 url = 'https://api.lyrics.ovh/v1/'+artist+'/'+song
 
-# get content from url
+#get content from url and convert to JSON format
 response = requests.get(url)
 json_data = json.loads(response.text)
 
-#create file and write lyrics
+#convert JSON format data to readable text
 with open ("lyrics.txt","w") as f:
     f.write(json_data['lyrics'])
 
